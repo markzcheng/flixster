9b0b9e92ac23ef13becf2bcb60956d4a21af63ab
Mark Cheng <markcheng@wireless-10-145-94-97.public.utexas.edu>
2019-02-04 19:31:09 -0600

    cocoa pods

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -49,11 +49,19 @@ class MoviesViewController: UIViewController, UITableViewDataSource,
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
         
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
-        cell.textLabel?.text = title
+        let synopsis = movie["overview"] as! String
+        
+        cell.titleLabel.text = title
+        cell.synopsisLabel.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
         return cell
     }
 
