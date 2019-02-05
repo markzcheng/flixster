5866bc3b659826ca62d47d026b12fdcad396a2a5
Mark Cheng <markcheng@wireless-10-145-94-97.public.utexas.edu>
2019-02-04 18:28:48 -0600

    tableview setup

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
-
+class MoviesViewController: UIViewController, UITableViewDataSource,
+    UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     var movies = [[String:Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view.
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -26,6 +33,8 @@ class MoviesViewController: UIViewController {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 
                 self.movies = dataDictionary["results"] as! [[String:Any]]
+                self.tableView.reloadData()
+                
                 // TODO: Get the array of movies
                 // TODO: Store the movies in a property to use elsewhere
                 // TODO: Reload your table view data
@@ -35,6 +44,18 @@ class MoviesViewController: UIViewController {
         task.resume()
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        cell.textLabel?.text = title
+        return cell
+    }
 
     /*
     // MARK: - Navigation
