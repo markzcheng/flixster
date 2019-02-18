9e583c638ba319e69a73e61e70327e67f76dd165
Mark Cheng <markcheng@wireless-10-148-182-114.public.utexas.edu>
2019-02-17 19:01:23 -0600

    collection view setup

---
+++ MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Mark Cheng on 2/17/19.
+//  Copyright © 2019 Mark Cheng. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
